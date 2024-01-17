8ab5c24817e3d3d501d28ad89c3f425333ccbf2b
Adolfo Sanz De Diego <asanzdiego@gmail.com>
2024-01-17 22:50:39 +0100

    release 0.9.6

--- js/extension.js
+++ js/extension.js
@@ -44,52 +44,8 @@ function addFocusListenerToPasswordField(field) {
             }
         });
 
-        const iframes = document.querySelectorAll('iframe');
-
-        if (SHOW_IFRAME_MESSAGE && iframes && iframes.length > 0) {
-
-            SHOW_IFRAME_MESSAGE = false;
-
-            const popover = createPopover();
-            const closeButton = createCloseButton(popover);
-
-            addTitle(popover);
-
-            const paragraph1 = document.createElement('p');
-            paragraph1.innerHTML = "PMG doesn't work with iframes. You can open the iframe directly:";        
-            popover.appendChild(paragraph1);
-
-            const link1 = document.createElement('a');
-            link1.href = iframes[0].src;
-            link1.text = iframes[0].src;
-            popover.appendChild(link1);
-
-            const paragraph2 = document.createElement('p');
-            paragraph2.innerHTML = "Or you can generate your password from PMG home:";        
-            popover.appendChild(paragraph2);
-
-            const link2 = document.createElement('a');
-            link2.href = 'https://pasmangen-extension.github.io/';
-            link2.text = 'https://pasmangen-extension.github.io/';
-            popover.appendChild(link2);
-
-            popover.style.width = '30rem';
-            popover.style.left = '50%';
-            popover.style.transform = 'translateX(-50%)';
-            popover.style.top = '50%';
-            popover.style.transform = 'translateY(-50%)';
-
-            document.body.appendChild(popover);
-
-            closeButton.addEventListener('click', function () {
-                popover.style.setProperty('display', 'none', 'important');
-            });
-        }
-
     };
 }
-
-let SHOW_IFRAME_MESSAGE = true;
 
 // Load when there are changes in the DOM
 const observer = new MutationObserver((mutations) => {
